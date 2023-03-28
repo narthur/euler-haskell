cb03cbb2d2b6b20c76ad61604a987caea4c9e8f5
Nathan Arthur <nathan@nathanarthur.com>
2023-03-28 10:57:35 -0400

    p2 pseudocode

--- problems/2/solve.hs
+++ problems/2/solve.hs
@@ -1,3 +1,18 @@
+-- this is a comment
+
+{-
+this is a multiline comment
+
+1,2,3,5,8,...
+
+l = [1,2]
+
+while l[-1] < 4000000:
+    l.append(l[-1] + l[-2])
+
+sum l
+-}
+
 solve = -1
 
 validate = solve == 4613732
