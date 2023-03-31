9c6351a0c3f5e4ff8bc9ef1d8e075dcd96aa9515
Nathan Arthur <nathan@nathanarthur.com>
2023-03-31 14:34:50 -0400

    solve problem 2

--- problems/2/solve.hs
+++ problems/2/solve.hs
@@ -11,8 +11,32 @@ while l[-1] < 4000000:
     l.append(l[-1] + l[-2])
 
 sum l
+
+---
+
+how would I do this in an immutable style?
+
+const limit = 4000000
+const fib = (a, b) => a < limit ? fib(b, a + b) : a
+
+function fib(acc = [1, 2]) {
+    if (acc[1] > limit) {
+        return acc
+    } else {
+        return fib([...acc, acc[-1] + acc[-2]])
+    }
+}
+
+const seq = fib()
+const sum = seq.reduce((acc, x) => acc + x, 0)
+
 -}
 
-solve = -1
+limit = 4000000
+fib l = if (last l) < limit then fib (l ++ [last l + last (init l)]) else l
+fibseq = fib [1,2]
+evens = filter even fibseq
+
+solve = sum evens
 
 validate = solve == 4613732
