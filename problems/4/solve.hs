9aeb09a4b241f47c3b7450fc8e8b554cccf3da9b
Nathan Arthur <nathan@nathanarthur.com>
2023-04-08 21:09:47 -0400

    start work on #4

---
+++ problems/4/solve.hs
@@ -0,0 +1,10 @@
+{-
+Write a function to check if a number is a palindrome
+Iterate through the multipliers and check if the product is a palindrome
+Iterate backwards to make sure the first match is the largest
+999 * 999
+999 * 998
+998 * 998
+998 * 997
+etc
+-}
